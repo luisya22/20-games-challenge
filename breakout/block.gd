89baad7bb61346c9fa381221c46d23370a2382ab
Luis Matos <lmatosgarcia2@gmail.com>
2025-10-14 22:51:56 -0400

    feat: looks and colors

--- breakout/block.gd
+++ breakout/block.gd
@@ -1,7 +1,18 @@
 class_name Block
 extends StaticBody2D
 
-@export var color: Color = Color.WHITE
+@export var row: int = 1
+
+var colors: Array = [
+	Color.WHITE,
+	Color("#1E90FF"), # Blue
+	Color("#00CC66"), # Green
+	Color("#FFD700"), # Yellow
+	Color("#FF8C00"), # Orange
+	Color("#FF4500"), # Red-orange
+	Color("#C71585"), # Magenta
+	Color("#ADFF2F")  # Lime
+]
 
 func _ready() -> void:
-	modulate = color
+	modulate = colors[row]
